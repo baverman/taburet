22b8ddb1462436fc58e8d9ab990790567bbb989a
Anton Bobrov <bobrov@vl.ru>
2010-06-12 22:16:51 +1100

    events and actions mock

--- src/taburet/accounting/ui.py
+++ src/taburet/accounting/ui.py
@@ -1,24 +1,39 @@
+# -*- coding: utf-8 -*-
+
 from flask import Module, abort, url_for, request 
 from couchdbkit import ResourceNotFound
 
 from .model import AccountsPlan, Account
-from taburet.ui import Form, TreeViewControl, TreeViewEndpointDataSource, jsonify
+from taburet.ui import (Window, Form, TreeViewControl, TreeViewEndpointDataSource,
+    jsonify, TextField)
 
 module = Module('taburet.accounting', url_prefix='/accounting')
 
 @module.route('/form/accounts-plan')
 def form_accounts_plan():
-    form = Form()
-    form.add_control(
-        TreeViewControl('accounts',
-            TreeViewEndpointDataSource(url_for('data_accounts'))))
+    window = Window()
     
-    return jsonify(form)
+    tree = TreeViewControl('accounts',
+            TreeViewEndpointDataSource(url_for('data_accounts')))
+    tree.root_title = u'Счета'
+    
+    form = Form('account', url_for('data_account'),[
+        TextField('id', 'id'),
+        TextField('name', u'Счет')
+    ])
+   
+    window.add(tree)
+    window.add(form)
+    
+    window.on(tree.selected_event()) \
+        .do(form.update_action())
+    
+    return jsonify(window)
 
 @module.route('/data/accounts')
 def data_accounts():
     root = request.args.get('root', None) #@UndefinedVariable
-    if not root:
+    if not root or root=='root':
         plan = AccountsPlan()
         accounts = plan.accounts()
     else:
@@ -31,8 +46,10 @@ def data_accounts():
         
     return jsonify([{'id':r._id, 'name':r.name if r.name else r._id} for r in accounts])
 
-@module.route('/data/account/<id>')
-def data_account(id):
+@module.route('/data/account/<id>', methods=('GET', 'POST'))
+@module.route('/data/account', methods=('GET', 'POST'))
+def data_account(id=None):
+    id = id or request.args.get('id') #@UndefinedVariable
     try:
         account = Account.get(id)
     except ResourceNotFound:
--- src/taburet/ui/__init__.py
+++ src/taburet/ui/__init__.py
@@ -1,29 +1,79 @@
 import flask
 
-class Form(object):
-    def __init__(self):
-        self.controls = []
+class Event(object):
+    def __init__(self, id, event):
+        self.id = id
+        self.event = event
+        self.actions = []
+        
+    def do(self, action):
+        '''
+        Assignd action to event
+        
+        @param action: Action
+        @return: Event
+        '''
+        self.actions.append(action)
+        
+        return self
         
     def as_jsonable(self):
-        return {'_type': 'Form', 'controls':[r.as_jsonable() for r in self.controls]}
+        return {'id':self.id, 'event':self.event,
+            'actions':[r.as_jsonable() for r in self.actions]}
 
-    def add_control(self, control):
-        self.controls.append(control)
+
+class Action(object):
+    def __init__(self, id, action):
+        self.id = id
+        self.action = action
+    
+    def as_jsonable(self):
+        return {'id':self.id, 'action':self.action}
+    
+
+class Window(object):
+    def __init__(self):
+        self.widgets = []
+        self.events = []
+        
+    def as_jsonable(self):
+        return {
+            'widgets':[r.as_jsonable() for r in self.widgets],
+            'events':[r.as_jsonable() for r in self.events]
+        }
+
+    def add(self, widget):
+        self.widgets.append(widget)
+        
+    def on(self, event):
+        '''
+        Assigns event to window
+        
+        @param event: Event
+        @return: Event
+        '''
+        self.events.append((event))
+        return event
 
 
 class TreeViewControl(object):
-    
-    def __init__(self, name, datasource):
+    def __init__(self, id, datasource):
         self.datasource = datasource
-        self.name = name
+        self.id = id
+        self.root_title = 'Root'
     
     def as_jsonable(self):
         return {
             '_type': 'TreeViewControl',
-            'name': self.name,
-            'datasource': self.datasource.as_jsonable()
-        } 
+            'id': self.id,
+            'datasource': self.datasource.as_jsonable(),
+            'root_title': self.root_title,
+        }
+        
+    def selected_event(self):
+        return Event(self.id, 'selected')
     
+
 class TreeViewEndpointDataSource(object):
     
     def __init__(self, endpoint):
@@ -32,6 +82,29 @@ class TreeViewEndpointDataSource(object):
     def as_jsonable(self):
         return {'_type':'TreeViewEndpointDataSource', 'endpoint':self.endpoint}
     
+
+class Form(object):
+    def __init__(self, id, endpoint, fields=[]):
+        self.id = id
+        self.endpoint = endpoint
+        self.fields = fields
+        
+    def as_jsonable(self):
+        return {'_type':'Form', 'id':self.id, 'endpoint':self.endpoint,
+            'fields':[r.as_jsonable() for r in self.fields]}
+        
+    def update_action(self):
+        return Action(self.id, 'update')
+
+
+class TextField(object):
+    def __init__(self, id, label):
+        self.id = id
+        self.label = label
+
+    def as_jsonable(self):
+        return {'_type':'TextField', 'id':self.id, 'label':self.label}
+
 
 def jsonify(*args, **kwargs):
     if len(args) == 1 and not kwargs:
