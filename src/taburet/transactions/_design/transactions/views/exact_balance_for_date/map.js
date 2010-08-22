e46fc98a4bedacd82a2f2df86d476a35b5624735
Anton Bobrov <bobrov@vl.ru>
2010-08-22 16:35:38 +1100

    removed uneeded transactions views

--- src/taburet/transactions/_design/transactions/views/exact_balance_for_date/map.js
+++
@@ -1,6 +0,0 @@
-function(doc) {
-	if (doc.doc_type && doc.doc_type == 'Transaction') {
-		emit([doc.date[0], doc.date[1], doc.date[2], doc.from_acc[doc.from_acc.length-1]], {kredit:doc.amount, debet:0})
-		emit([doc.date[0], doc.date[1], doc.date[2], doc.to_acc[doc.to_acc.length-1]], {kredit:0, debet:doc.amount})
-	}
-}
