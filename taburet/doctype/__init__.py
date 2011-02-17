c2549286ca729ae3a03ba26a5f1ff6544e7fa03a
Anton Bobrov <bobrov@vl.ru>
2011-02-17 19:58:42 +1000

    clean

--- taburet/doctype/__init__.py
+++ taburet/doctype/__init__.py
@@ -1,5 +1,2 @@
-import os.path
-from couchdbkit import FileSystemDocsLoader, ResourceConflict, Document
-
 def get_by_type(cls):
-    return cls.view('doctype/get', key=cls.__name__, include_docs=True)   
+    return cls.view('doctype/get', key=cls.__name__, include_docs=True)
